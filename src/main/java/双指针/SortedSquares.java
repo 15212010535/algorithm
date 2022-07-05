f08b9c321729d036fa15e18113ddbb8f81610c57
15212010535 <2278663531@qq.com>
2022-07-05 20:48:27 +0800

    力扣977

---
+++ src/main/java/双指针/SortedSquares.java
@@ -0,0 +1,55 @@
+package 双指针;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序
+ * <p>
+ * 实例：
+ * 输入：nums = [-4,-1,0,3,10]
+ * 输出：[0,1,9,16,100]
+ * 解释：平方后，数组变为 [16,1,0,9,100]
+ * 排序后，数组变为 [0,1,9,16,100]
+ */
+public class SortedSquares {
+    public static int[] sortedSquares(int[] nums) {
+//        return Arrays.stream(nums).map(operand -> operand * operand).sorted().toArray();
+        int n = nums.length;
+        int negative = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < 0) {
+                negative = i;
+            } else {
+                break;
+            }
+        }
+
+        int[] ans = new int[n];
+        int index = 0, i = negative, j = negative + 1;
+        while (i >= 0 || j < n) {
+            if (i < 0) {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            } else if (j == n) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            }
+            ++index;
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] ans = sortedSquares(nums);
+        Arrays.stream(ans).forEach(System.out::println);
+    }
+}
