5bcb5bb40b7e4f623fcd37a4fa337b474d7e4ae6
qjx <2278663531@qq.com>
2023-06-08 23:42:04 +0800

    链表相交

---
+++ src/main/java/code/link/GetIntersectionNode.java
@@ -0,0 +1,16 @@
+package code.link;
+
+/**
+ * 链表相交
+ */
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
