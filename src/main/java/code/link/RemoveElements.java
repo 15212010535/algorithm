6368fad0560a93835964dfced6795877d6d66275
qjx <2278663531@qq.com>
2023-06-08 22:35:56 +0800

    移除链表元素

---
+++ src/main/java/code/link/RemoveElements.java
@@ -0,0 +1,27 @@
+package code.link;
+
+import offer.ListNode;
+
+/**
+ * 移除链表元素
+ */
+public class RemoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val)
+            head = head.next;
+        if (head == null)
+            return head;
+
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
