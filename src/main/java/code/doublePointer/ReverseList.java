78c54c57869814aaae71be7bc312458b289ab5a1
qjx <2278663531@qq.com>
2023-06-12 22:02:04 +0800

    反转链表

---
+++ src/main/java/code/doublePointer/ReverseList.java
@@ -0,0 +1,21 @@
+package code.doublePointer;
+
+import offer.ListNode;
+
+/**
+ * 反转链表
+ */
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
