69f1defa4aabc568d5485562ee43e181df8afcef
qjx <2278663531@qq.com>
2023-06-13 21:47:40 +0800

    前k个高频元素

---
+++ src/main/java/code/stackQueue/TopKFrequent1.java
@@ -0,0 +1,28 @@
+package code.stackQueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+/**
+ * 前k个高频元素
+ */
+public class TopKFrequent1 {
+    public int[] topKFrequent1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<int[]> queue = new PriorityQueue<>(map.size(), (o1, o2) -> o2[1] - o1[1]);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            queue.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = Objects.requireNonNull(queue.poll())[0];
+        }
+        return ans;
+    }
+
+}
