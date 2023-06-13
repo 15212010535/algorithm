09435af1679c7c8b842fc4bdae16f7172d326849
qjx <2278663531@qq.com>
2023-06-14 00:03:46 +0800

    填充每个节点的下一个右侧节点指针

---
+++ src/main/java/code/tree/Connect.java
@@ -0,0 +1,53 @@
+package code.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+/**
+ * 填充每个节点的下一个右侧节点指针
+ */
+public class Connect {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                Node node = queue.poll();
+                if(size - 1>0) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+                size--;
+            }
+        }
+        return root;
+    }
+}
