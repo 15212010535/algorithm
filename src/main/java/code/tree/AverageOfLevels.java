6b3073a6162433946121820ab4993359e39ba829
qjx <2278663531@qq.com>
2023-06-13 23:29:37 +0800

    二叉树的层平均值

---
+++ src/main/java/code/tree/AverageOfLevels.java
@@ -0,0 +1,36 @@
+package code.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层平均值
+ */
+public class AverageOfLevels {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            int cur = size;
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+                size--;
+            }
+            res.add(sum / cur);
+        }
+        return res;
+    }
+}
