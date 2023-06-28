f0423bf5bfd9b6d65b0e2853f82e35d50e19efd2
qjx <2278663531@qq.com>
2023-06-28 21:17:16 +0800

    跳跃游戏 II

---
+++ src/main/java/code/greedy/Jump.java
@@ -0,0 +1,20 @@
+package code.greedy;
+
+/**
+ *
+ */
+public class Jump {
+    public int jump(int[] nums) {
+        int result = 0;
+        int end = 0;
+        int temp = 0;
+        for (int i = 0; i <= end && end < nums.length - 1; i++) {
+            temp = Math.max(temp, i + nums[i]);
+            if (i == end) {
+                end = temp;
+                result++;
+            }
+        }
+        return result;
+    }
+}
