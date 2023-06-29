d36aaf2ace5512f589fb516ef5d94c5e509febc7
qjx <2278663531@qq.com>
2023-06-29 09:26:01 +0800

    用最少数量的箭引爆气球

--- src/main/java/code/greedy/FindMinArrowShots.java
+++ src/main/java/code/greedy/FindMinArrowShots.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  */
 public class FindMinArrowShots {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> a[0] - b[0]);
+//        Arrays.sort(points, (a, b) -> a[0] - b[0]);
         Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
         int count = 1;
         for (int i = 1; i < points.length; i++) {
