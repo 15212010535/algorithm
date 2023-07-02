119bbf45022ab21d83d219235f10123fe1d30956
qjx <2278663531@qq.com>
2023-07-02 21:28:57 +0800

    最后一块石头的重量 II

--- src/main/java/code/dp/LastStoneWeightII.java
+++ src/main/java/code/dp/LastStoneWeightII.java
@@ -9,7 +9,7 @@ public class LastStoneWeightII {
     public int lastStoneWeightII(int[] stones) {
         int sum = Arrays.stream(stones).sum();
         int target = sum >> 1;
-        int[] dp = new int[stones.length + 1];
+        int[] dp = new int[target + 1];
         for (int i = 0; i < stones.length; i++) {
             for (int j = target; j >= stones[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
