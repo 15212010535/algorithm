401d8519cbf2477fd973c772be1f6b68de89e269
qjx <2278663531@qq.com>
2023-07-03 10:22:38 +0800

    爬楼梯

--- src/main/java/code/dp/ClimbStairs.java
+++ src/main/java/code/dp/ClimbStairs.java
@@ -13,4 +13,17 @@ public class ClimbStairs {
         }
         return dp[n];
     }
+
+    public int climbStairsTwo(int n) {
+        int[] dp = new int[n + 1];
+        int m = 2;
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i >= j)
+                    dp[i] += dp[i - j];
+            }
+        }
+        return dp[n];
+    }
 }
